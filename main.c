c5484d5a74b2b8e62e32627597a724e321bbf51b
Shayan Azmoodeh <shkev.az@gmail.com>
2020-02-13 17:23:15 -0800

    allowing input from command line for learning_rate and num_iterations

--- main.c
+++ main.c
@@ -8,12 +8,17 @@ int main(int argc, char* argv[])
 {
   //input data
   float x[10] = {1,2,3,4,5,6,7,8,9,10};
-  float y[10] = {2,4,6,9,15,12,15,17,20,30};
+  float y[10] = {1,2,3,4,5,6,7,8,9,10};
 
   //hyperparams
-  float learning_rate = 0.01;
-  int num_iterations = 100;
+  /*float learning_rate = 0.0001;
+  int num_iterations = 1000;*/
 
+  //reading in input from command line
+  float learning_rate = atof(argv[1]);
+  int num_iterations = atoi(argv[2]);
+  printf("%f %d\n", learning_rate, num_iterations);
+  
   //initializing parameters
   srand(time(NULL));
   float w = rand() % 2;
@@ -30,10 +35,10 @@ int main(int argc, char* argv[])
   printf("Predicted Value For 10 is %f \n", predict(10, w, b));
 
   //printing cost values over iterations
-  int count;
+  /*int count;
   for (count = 0; count < num_iterations; count++)
   {
     printf("Cost %d %f \n", count, cost[count]);
-  }
+  }*/
   exit(EXIT_SUCCESS);
 }
