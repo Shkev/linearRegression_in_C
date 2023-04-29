220cedd220169f7decbdf3117651cfc3639f4577
Shayan Azmoodeh <39930235+Shkev@users.noreply.github.com>
2023-04-29 17:11:46 -0500

    Cleaning code in linRegression.c

--- linRegression.c
+++ linRegression.c
@@ -3,37 +3,51 @@
 #include <math.h>
 #include "linRegression.h"
 
+/**
+* Update weight and bias using gradient descent.
+* Uses all of data to compute gradient
+* Returns loss
+*/
 float batch_gradient_descent(float x[], float y[], int num_data, float learning_rate, float* w, float* b)
 {
-  float db = 0.0; //derivative of cost func with respect to bias
-  float dw = 0.0; //derivative of cost func with respect to weights
-  float cost = 0.0;
-  int count;
-  for (count = 0; count < num_data; count++)
+  float db = 0.0; // derivative of cost func with respect to bias
+  float dw = 0.0; // derivative of cost func with respect to weights
+  float loss = 0.0;
+	// go through all data points to compute loss
+  for (size_t count = 0; count < num_data; count++)
   {
-    cost += pow((((*w) * x[count] + *b) - y[count]), 2) / (2 * num_data); //equation to find cost
-    db += ((*w * x[count] + *b) - y[count]) / num_data;
-    dw += ((*w * x[count] + *b) - y[count]) * x[count] / num_data;
+    loss += pow(((*w * x[count] + *b) - y[count]), 2); // equation to find loss/empirical risk
+    db += (*w * x[count] + *b) - y[count];
+    dw += ((*w * x[count] + *b) - y[count]) * x[count];
   }
-
-  //updating weight and bias
+	loss /= 2*num_data;
+	db /= num_data;
+	dw /= num_data;
+  // updating weight and bias using gradient descent
   *w = *w - (learning_rate * dw);
   *b = *b - (learning_rate * db);
-  return cost;
+	
+  return loss;
 }
 
+/**
+* Perform regression on given dataset (x, y) using num_iterations number of iterations.
+* Returns the final loss after training is complete.
+*/
 float* regression(float x[], float y[], int num_data, float learning_rate, int num_iterations, float* w, float* b)
 {
-  static float* cost;
-  cost = malloc(sizeof(int) * num_iterations);
-  int count;
-  for (count = 0; count < num_iterations; count++)
+  float* loss;
+  loss = malloc(sizeof(int) * num_iterations);
+  for (size_t count = 0; count < num_iterations; count++)
   {
-    cost[count] = batch_gradient_descent(x, y, num_data, learning_rate, w, b);
+    loss[count] = batch_gradient_descent(x, y, num_data, learning_rate, w, b);
   }
-  return cost;
+  return loss;
 }
 
+/**
+* Predict output for given input x with given weights and biases
+*/
 float predict(float x, float w, float b)
 {
   float y = w * x + b;
